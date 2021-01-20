846e40897df51f1ab3eadf6bba1e6cff006d8d04
Julia Prein <jprein@me.com>
2021-01-20 16:42:03 +0100

    create function for division (for randomization)

---
+++ src/js/divideWithRemainder.js
@@ -0,0 +1,12 @@
+export default (dividend, divisor) => {
+  let quotient = 0;
+  let remainder = 0;
+  if (dividend % divisor === 0) {
+    quotient = dividend / divisor;
+    // keep remainder = 0
+  } else {
+    quotient = Math.floor(dividend / divisor);
+    remainder = dividend % divisor;
+  }
+  return { quotient, remainder };
+};
