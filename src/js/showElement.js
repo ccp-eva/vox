9ad6623c04dfe90902dd40fd211348f761e8c1eb
Julia Prein <jprein@me.com>
2021-01-21 09:24:58 +0100

    adjust function, now  elems can appear >1 in list

--- src/js/showElement.js
+++ src/js/showElement.js
@@ -1,6 +1,6 @@
 export default (elements, trialCount) => {
   for (let i = 0; i < elements.length; i++) {
-    if (i === trialCount) {
+    if (elements[i] === elements[trialCount]) {
       elements[i].setAttribute('visibility', 'visible');
     } else {
       elements[i].setAttribute('visibility', 'hidden');
