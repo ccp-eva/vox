345a10ec7f5eb4a523c2484710937ea64a5e8191
Julia Prein <jprein@me.com>
2021-01-28 08:19:09 +0100

    simplify setCenter functions

--- src/js/setEyeCenter.js
+++ src/js/setEyeCenter.js
@@ -1,16 +1,8 @@
 export default (target, newCoords) => {
-  const oldX = parseFloat(target.getAttribute('cx'));
-  const oldY = parseFloat(target.getAttribute('cy'));
-  const newX = newCoords.x;
-  const newY = newCoords.y;
-
   function moveCenter() {
-    const nextX = oldX + (newX - oldX);
-    const nextY = oldY + (newY - oldY);
-    target.setAttribute('cx', nextX);
-    target.setAttribute('cy', nextY);
+    target.setAttribute('cx', newCoords.x);
+    target.setAttribute('cy', newCoords.y);
     requestAnimationFrame(moveCenter);
   }
-
   requestAnimationFrame(moveCenter);
 };
--- src/js/setTargetCenter.js
+++ src/js/setTargetCenter.js
@@ -1,26 +1,7 @@
 export default (target, newViewBox) => {
-  const oldX = parseFloat(target.getAttribute('viewBox').split(' ')[0]);
-  const oldY = parseFloat(target.getAttribute('viewBox').split(' ')[1]);
-  const oldWidth = parseFloat(target.getAttribute('viewBox').split(' ')[2]);
-  const oldHeight = parseFloat(target.getAttribute('viewBox').split(' ')[3]);
-
-  const newX = parseFloat(newViewBox.split(' ')[0]);
-  const newY = parseFloat(newViewBox.split(' ')[1]);
-  const newWidth = parseFloat(newViewBox.split(' ')[2]);
-  const newHeight = parseFloat(newViewBox.split(' ')[3]);
-
-  // recursive function for moving the viewBox
   function moveViewBox() {
-    const nextViewBox = [
-      oldX + (newX - oldX),
-      oldY + (newY - oldY),
-      oldWidth + (newWidth - oldWidth),
-      oldHeight + (newHeight - oldHeight),
-    ];
-    target.setAttribute('viewBox', nextViewBox.join(' '));
+    target.setAttribute('viewBox', newViewBox);
     requestAnimationFrame(moveViewBox);
   }
-
-  // call recursive function once to start process
   requestAnimationFrame(moveViewBox);
 };
