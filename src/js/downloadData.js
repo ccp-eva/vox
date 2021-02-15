61cf5aa89065bd56112e25e6a19a0211b2758092
Julia Prein <jprein@me.com>
2021-02-15 13:19:56 +0100

    use blob for download

--- src/js/downloadData.js
+++ src/js/downloadData.js
@@ -1,14 +1,11 @@
 export default (safe, ID) => {
-  const toSave = JSON.stringify(safe, null, 1);
+  const toSave = new Blob([JSON.stringify(safe, null, 1)]);
   const day = new Date().toISOString().substr(0, 10);
   // note: it's UTC time (so for germany add +1)
   const time = new Date().toISOString().substr(11, 8);
-  console.log(time);
 
   const hiddenElement = document.createElement('a');
-  hiddenElement.href = `data:text/json;charset=utf-8,${encodeURI(toSave)}`;
-  hiddenElement.target = '_blank';
+  hiddenElement.href = window.URL.createObjectURL(toSave);
   hiddenElement.download = `gazefollowing-${ID}-${day}-${time}.json`;
   hiddenElement.click();
-  console.log('downloaded data');
 };
